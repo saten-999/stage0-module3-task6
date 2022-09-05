c675566f7e22e4ece104f1a681bf95509e675ac5
Abrahamyan <asatenik@mgc.mentorg.com>
2022-09-05 10:52:54 +0400

    done

--- src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
+++ src/main/java/lang/print/gaps/task6/FloatTypeCasting.java
@@ -2,5 +2,8 @@ package lang.print.gaps.task6;
 
 public class FloatTypeCasting {
     public void roundNumber(float numberToBeRounded) {
+
+        int f = (int) Math.round(numberToBeRounded);
+        System.out.println(f);
     }
 }
